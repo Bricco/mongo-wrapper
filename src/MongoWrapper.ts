36d279d67e6df6b3d2642b46d94cba0df3c2cb2e
Tom Honig <tom.honig@bricco.se>
2025-08-18 15:24:53 +0200

    debug

--- src/MongoWrapper.ts
+++ src/MongoWrapper.ts
@@ -132,9 +132,14 @@ export default class MongoWrapper<T extends Document = Document> {
     const time = performance.now();
 
     // // Pre-operation logging
-    // if (this.options.debug) {
-    //   debug(this.options.collection, method, 'START', { args, options });
-    // }
+    if (this.options.debug) {
+      debug({
+        name: this.options.collection,
+        method,
+        status: 'START',
+        parameters: { args, options },
+      });
+    }
 
     try {
       if (options?.cache === false || !this.cache || isMutation) {
